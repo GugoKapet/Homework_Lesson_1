e11700d7cadc8a675eb58414db01bc1a7f8af705
Карэн Саркисян <gugo@MacBook-Pro-Karen.local>
2022-10-16 23:51:34 +0200

    added Task One

---
+++ Task_One/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.Write("большее число - ");
+    Console.Write(a);
+    Console.Write(", меньшее число - ");
+    Console.WriteLine(b);
+}
+else
+    if (a < b)
+    {
+        Console.Write("большее число - ");
+        Console.Write(b);
+        Console.Write(", меньшее число - ");
+        Console.WriteLine(a); 
+    }
+    else
+    {
+        Console.Write("числа равны");
+    }
