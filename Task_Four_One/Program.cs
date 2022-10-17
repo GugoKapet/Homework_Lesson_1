e7997e405b290d8ef61034137d6cc984e8626028
Карэн Саркисян <gugo@MacBook-Pro-Karen.local>
2022-10-17 22:54:47 +0200

    added Task Four One with second decision

---
+++ Task_Four_One/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int even;
+while (count <= number)
+{
+    if (count % 2 == 0)
+    {
+        even = count;
+        Console.WriteLine(even + ", ");
+    }
+    count++;
+}
