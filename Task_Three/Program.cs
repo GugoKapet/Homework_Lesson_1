95412600456a754c2b65172a3674625eb4fb7d13
Карэн Саркисян <gugo@MacBook-Pro-Karen.local>
2022-10-17 19:05:27 +0200

    added Task Three

---
+++ Task_Three/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int resiedue = a % 2;
+if (resiedue == 1)
+{
+    Console.Write("число является нечётным");
+}
+else
+{
+    Console.Write("число является чётным");
+}
